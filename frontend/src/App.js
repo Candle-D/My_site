71009151e78df21695b37822c4f5a4de9123a3aa
Candle Dcosta <aqsakibria0@gmail.com>
2022-04-10 11:32:59 +0530

    vid_12

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,37 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
+
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">My_Site</a>
-      </header>
-      <main>list items</main>
-    </div>
+    <BrowserRouter>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>MY_SITE</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+
+        <footer>
+          <div className="text-center">All right reserved</div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
